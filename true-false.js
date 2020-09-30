e2054c0f917c8882efa0a472df66d92b9146d41a
atika-yasmin <atika.yasmin.19@gmail.com>
2020-09-30 12:04:07 +0600

    true-false is commited

---
+++ true-false.js
@@ -0,0 +1,17 @@
+//falsy
+//false
+// 0
+// ""
+// undefined
+//null
+//NaN
+
+//truthy
+//'0' ' ' [] {} 'false'
+let name = 0;
+if(name || name == 0){
+    console.log("conddition is true")
+}
+else{
+    console.log("conddition is false")
+}
