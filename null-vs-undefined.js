b4a53cdb7f5ef7a41e700d3deed845423a872b39
atika-yasmin <atika.yasmin.19@gmail.com>
2020-10-01 11:09:27 +0600

    null-vs-undefined is finished

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+//undefined
+let atika;
+function add(num1, num2){
+   console.log(num1 + num2);
+   return;
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add(13);
+
+const boy = {name:"ayaan", phone:01778894334}
+console.log(boy.id);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
